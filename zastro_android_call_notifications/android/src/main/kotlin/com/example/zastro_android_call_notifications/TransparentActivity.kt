4107fbd00e3c39a0f40c97ec8356a0ab92d91f3c
zaidTech-source <zaidtech002@gmail.com>
2025-04-26 17:27:20 +0530

    Solving notif UI things

--- zastro_android_call_notifications/android/src/main/kotlin/com/example/zastro_android_call_notifications/TransparentActivity.kt
+++ zastro_android_call_notifications/android/src/main/kotlin/com/example/zastro_android_call_notifications/TransparentActivity.kt
@@ -60,8 +60,8 @@ class TransparentActivity : Activity() {
         val context = applicationContext
         val launchIntent = context.packageManager.getLaunchIntentForPackage(context.packageName)?.apply {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                    Intent.FLAG_ACTIVITY_CLEAR_TOP or
-                    Intent.FLAG_ACTIVITY_SINGLE_TOP or
+//                    Intent.FLAG_ACTIVITY_CLEAR_TOP or
+//                    Intent.FLAG_ACTIVITY_SINGLE_TOP or
                     Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED
             putExtra("message_data_in_string", messageDataInString)
             putExtra("key", intent.action)
