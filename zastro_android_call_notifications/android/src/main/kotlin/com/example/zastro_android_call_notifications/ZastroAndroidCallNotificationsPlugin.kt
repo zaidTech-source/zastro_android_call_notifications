64cdb53aba4231a8e3a7677e8c30430555bb3c89
zaidTech-source <zaidtech002@gmail.com>
2025-04-28 15:57:40 +0530

    Solving notif UI things

--- zastro_android_call_notifications/android/src/main/kotlin/com/example/zastro_android_call_notifications/ZastroAndroidCallNotificationsPlugin.kt
+++ zastro_android_call_notifications/android/src/main/kotlin/com/example/zastro_android_call_notifications/ZastroAndroidCallNotificationsPlugin.kt
@@ -23,9 +23,6 @@ import io.flutter.plugin.common.MethodChannel.Result
 import org.json.JSONObject
 import android.content.BroadcastReceiver
 import android.content.IntentFilter
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.embedding.engine.FlutterEngineCache
-import io.flutter.embedding.engine.dart.DartExecutor
 
 
 class ZastroAndroidCallNotificationsPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
@@ -38,26 +35,10 @@ class ZastroAndroidCallNotificationsPlugin : FlutterPlugin, MethodCallHandler, A
   private lateinit var callOngoingReceiver: CallOngoingTimeNotificationReceiver
   private var activity: Activity? = null
   private var latestNotificationData: Map<String, Any?>? = null
-  private lateinit var flutterEngine: FlutterEngine
-
-  companion object {
-    private const val FLUTTER_ENGINE_NAME = "ZastroFlutterEngine"
-  }
 
   override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
     Log.d("FlutterCallkitIncoming", "onAttachedToEngine called")
-
     context = binding.applicationContext
-
-    // Cache the FlutterEngine for reuse
-    var flutterEngine = FlutterEngineCache.getInstance().get(FLUTTER_ENGINE_NAME)
-    if (flutterEngine == null) {
-      flutterEngine = FlutterEngine(context)
-      flutterEngine.navigationChannel.setInitialRoute("/")
-      flutterEngine.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
-      FlutterEngineCache.getInstance().put(FLUTTER_ENGINE_NAME, flutterEngine)
-    }
-
     channel = MethodChannel(binding.binaryMessenger, "Chat notifications")
     channel.setMethodCallHandler(this)
 
@@ -329,7 +310,6 @@ class ZastroAndroidCallNotificationsPlugin : FlutterPlugin, MethodCallHandler, A
     channel.setMethodCallHandler(null)
     callTimerChannel.setMethodCallHandler(null)
     ongoingCallChannel.setMethodCallHandler(null)
-    FlutterEngineCache.getInstance().remove(FLUTTER_ENGINE_NAME)
     MethodChannelHelper.dispose()
   }
 }
